0303b4592c75658f6a3e8bc29c2afa27508c8850
junge <juncr.feng@gmail.com>
2019-03-22 18:22:44 +0800

    new index blade

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -6,8 +6,8 @@
 
             <div class="col-md-9" style="border:1px solid">qwe</div>
             <div class="col-md-3" >
-                <div class="box mb-2" style="border:1px solid">123</div>
-                <div class="box" style="border:1px solid">123</div>
+                <div class="box mb-2" style="border:1px solid">1231</div>
+                <div class="box" style="border:1px solid">1231</div>
             </div>
         </div>
     </div>
